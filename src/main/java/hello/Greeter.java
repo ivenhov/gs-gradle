a461357fddd54055684c02479604317f25b7ced6
ivenhov <iwan.daniel@gmail.com>
2017-03-30 21:02:03 +0100

    Added Java hello classes

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright Object Matrix 2017
+ */
+
+package hello;
+
+/**
+*
+* @author daniel
+*/
+public class Greeter {
+
+    public String sayHello() {
+        return "Hello world";
+    }
+
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright Object Matrix 2017
+ */
+
+package hello;
+
+/**
+*
+* @author daniel
+*/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
