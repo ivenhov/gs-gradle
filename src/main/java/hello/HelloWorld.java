8de5b4332ae8f04490a6b0cc6d46b2ceb145c9b6
ivenhov <iwan.daniel@gmail.com>
2017-03-30 21:58:48 +0100

    Added JodaTime dependency

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -8,10 +8,14 @@ package hello;
 *
 * @author daniel
 */
-public class HelloWorld {
+import org.joda.time.LocalTime;
 
-    public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
-    }
+public class HelloWorld {
+  public static void main(String[] args) {
+    LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
+
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
 }
